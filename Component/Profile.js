554f0fccff0f55e55291cf303fb21a72dae51ec2
Shuyuan2046 <72179897+Shuyuan2046@users.noreply.github.com>
2021-11-10 13:32:54 -0500

    Update Profile.js

--- Component/Profile.js
+++ Component/Profile.js
@@ -1,83 +1,26 @@
 import React from 'react';
 import { Button, View, Text, StyleSheet, TextInput, Alert, Pressable} from 'react-native';
 import {useState, useEffect} from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ExerciseLevelTest from './ExerciseLevelTest'
+//import AsyncStorage from '@react-native-async-storage/async-storage';
+import ValueProvider from './ValueContext';
 
-const Profile = (props) => {
+const Profile = ({navigation, route}) => {
   const [info, setInfo] = useState({name:'',email:''});
   const [name, setName] = useState('');
   const [age,setAge] = useState('')
   const [breed,setBreed] = useState('')
   const [submitted, setSubmitted] = useState(false);
 
-
   const onPressHandler = () => {
-    if (Number.isInteger(age)){
-        setSubmitted(!submitted);
-    } else {
-        Alert.alert('Warning', 'The age must be a number', [
-          {text: 'OK'}])
-    }
+    setSubmitted(!submitted);
   }
-
-    useEffect(() => {getData()}
-           ,[])
-
-    const getData = async () => {
-        try {
-          // the '@profile_info' can be any string
-          const jsonValue = await AsyncStorage.getItem('@profile_info')
-          let data = null
-          if (jsonValue!=null) {
-            data = JSON.parse(jsonValue)
-            setInfo(data)
-            setName(data.name)
-            setAge(data.age)
-            setBreed(data.breed)
-            console.log('just set Info, Name, Age and Breed')
-          } else {
-            console.log('just read a null value from Storage')
-            setInfo({})
-            setName("")
-            setAge("")
-            setBreed("")
-          }
-
-        } catch(e) {
-          console.log("error in getData ")
-          console.dir(e)
-          // error reading value
-        }
-  }
-
-  const storeData = async (value) => {
-    try {
-      const jsonValue = JSON.stringify(value)
-      await AsyncStorage.setItem('@profile_info', jsonValue)
-      console.log('just stored '+jsonValue)
-    } catch (e) {
-      console.log("error in storeData ")
-      console.dir(e)
-      // saving error
-    }
-}
-    const clearAll = async () => {
-      try {
-        console.log('in clearData')
-        await AsyncStorage.clear()
-      } catch(e) {
-        console.log("error in clearData ")
-        console.dir(e)
-        // clear error
-      }
-    }
-
-
 
     return(
         <View style={styles.container}>
           <Text>Please enter your pet information in the following lines: </Text>
-          <View>
           <TextInput
               multiline
               style={styles.input}
@@ -93,53 +36,31 @@ const Profile = (props) => {
               onChangeText={text => {setAge(text)}}
               value={age}
           />
+
           <TextInput
               style={styles.input}
               placeholder="pet breed"
               onChangeText={text => {setBreed(text)}}
               value={breed}
           /> 
-          </View>
-         
-         <View style={{backgroundColor: 'darkcyan'}}>
+          {/* <ValueProvider value={breed}>
+              <ExerciseLevelTest />
+          </ValueProvider> */}
+
+          <View style={{justifyContent: 'center', alignItems: 'center'}}>
             <Button
-                color='white'
-                title='Submit'
-                onPress = {() => {
-                console.log("saving profile");
-                const theInfo = {name:name,age:age,breed:breed}
-                console.log(`theInfo=${theInfo}`)
-                setInfo(theInfo)
-                console.log('data='+JSON.stringify(theInfo))
-                storeData(theInfo)
-            }}
-          />
-         </View>
+              color={submitted? 'green': 'royalblue'}
+              title={submitted ? 'Clear' : 'Submit'}
+              onPress={onPressHandler}
+              />
+          </View>   
           
-        <View style={styles.space} />
-
-        <View style={{backgroundColor: 'gray'}}>
-          <Button
-            color='white' title='Clear memory'
-            onPress = {() => {
-            console.log('clearing memory');
-            clearAll()
-            }}
-          />
+          {/* {submitted? 
+            
+          : 
+          null} */}
+          
         </View>
-
-        <View style={styles.space} />
-        
-        <View style={{backgroundColor: 'lightsteelblue'}}>
-          <Button
-            color='white' title='Load Profile from Memory'
-            onPress = {() => {
-            console.log('loading profile');
-            getData()
-            }}
-          />
-        </View>
-      </View>
     ) 
 }
 
@@ -164,6 +85,16 @@ const styles = StyleSheet.create({
     width: 10,
     height: 10,
   },
+
+  button: {
+    width: 80,
+    height: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    fontSize: 20,
+    marginBottom: 10,
+  }
 })
 
-export default Profile;
+export default Profile
